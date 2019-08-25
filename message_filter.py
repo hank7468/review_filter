7488d26767d1aa51222d4c430338f65566458b11
Hank Lee <hank7468@gmail.com>
2019-08-25 14:25:37 +0800

    add good filter

--- message_filter.py
+++ message_filter.py
@@ -1,10 +1,20 @@
-data = []
-num_character = input('please enter the maximum characters number used in the review: ')
-num_character = int(num_character)
+all_data = [] # Collect all data in this list
+i = 0 # i = data counts
 with open('reviews.txt', 'r') as f:
-	for message in f:
-		if len(message) < num_character:
-			print(message)
-			print('------------------------------------------------------')
-			data.append(message.strip())
-print('there are', len(data), 'reviews which is less than', num_character, 'characters.')
+	for line in f:
+		all_data.append(line.strip())
+		i += 1
+		if i % 1000 == 0:
+			print(i)
+
+filtered_data = []
+for line in all_data: # "line" is only used in current for loop.
+	if len(line) <= 100:
+		filtered_data.append(line.strip())
+print('there are', len(filtered_data), 'datas with less than or equal to 100 characters.')
+
+good_data = []
+for line in all_data:
+	if 'good' in line:
+		good_data.append(line.strip())
+print('there are', len(good_data), 'datas mention "good".')
